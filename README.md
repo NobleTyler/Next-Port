8a3f05e57304e48ca593c4457d35f7621c32b33f
Tyler Noble <tyler.noble@telna.com>
2021-04-15 17:22:46 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Next-Port
-My new portfolio made with next.js
+# Portfolio
+My new portfolio. Now with more next.
+
+
+## Still under construction.
+
+### Useful Guides and APIs
+- https://nextjs.org/learn
+
+I should add calendso to this.
---
+++ components/ProfilePic.js
@@ -0,0 +1,11 @@
+import Image from 'next/Image'
+
+const ProfilePic = () =>{
+    <Image
+    src="../public/images.profile.jpg"
+    height={144}
+    width={144}
+    alt="Tyler Noble's Profile Picture"
+    />
+}
+export default ProfilePic
